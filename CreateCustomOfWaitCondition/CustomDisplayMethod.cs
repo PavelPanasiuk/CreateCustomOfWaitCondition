e0183f8e5e9d7eb8487aa829be435a1fa597f1b4
Pavel Panasiuk <pavel.panasiuk.at@gmail.com>
2020-12-24 14:39:33 +0300

    First try of realize custom method

---
+++ CreateCustomOfWaitCondition/CustomDisplayMethod.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace CreateCustomOfWaitCondition
+{
+    public class CustomDisplayMethod
+    {
+
+        //Method without explisit wait
+        public bool FirstTryIsElementDisplay(string webpath, IWebDriver driver, string urlForSearchElement)
+        {
+            bool result = false;
+            driver.Navigate().GoToUrl(urlForSearchElement);
+
+            try
+            {
+                result = driver.FindElement(By.XPath(webpath)).Displayed;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return result;
+        }
+
+        //Method with explicit wait.
+        public bool SecondTryIsElementDisplay(string webpath, IWebDriver driver, string urlForSearchElement)
+        {
+            bool result = false;
+            driver.Navigate().GoToUrl(urlForSearchElement);
+
+            try
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                IWebElement webElement = wait.Until<IWebElement>((d) => d.FindElement(By.XPath(webpath)));
+                result = webElement.Displayed;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return result;
+        }
+    }
+}
--- CreateCustomOfWaitCondition/TestCustomDisplayAndEnableClass.cs
+++ CreateCustomOfWaitCondition/TestCustomDisplayAndEnableClass.cs
@@ -9,18 +9,46 @@ namespace CreateCustomOfWaitCondition
     [TestFixture]
     public class Tests
     {
+        private IWebDriver _chromeDriver = new ChromeDriver();
+        private CustomDisplayMethod _display = new CustomDisplayMethod();
+        private CustomEnableMethod _enable = new CustomEnableMethod();
 
         [SetUp]
         public void Setup()
         {
-            
+            _chromeDriver.Navigate().GoToUrl("https://www.google.com/");
         }
 
         [Test]
-        public void Test1()
+        public void TestingDisplayedMethod()
         {
+            string xPath = "//input[@name='q']";
+            _chromeDriver.FindElement(By.XPath(xPath));
+
+            var result = _display.FirstTryIsElementDisplay(xPath, _chromeDriver, _chromeDriver.Url);
+            if (result == false)
+            {
+                throw new Exception("This element didn't display.");
+            }
         }
 
+        [Test]
+        public void TestingEnableMethod()
+        {
+            string xPath = "//input[@class='gNO89b'][1]";
+            _chromeDriver.FindElement(By.XPath(xPath));
 
+            var result = _enable.CanWeClick(xPath, _chromeDriver, _chromeDriver.Url);
+            if (result == false)
+            {
+                throw new Exception("This element didn't enable.");
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _chromeDriver.Quit();
+        }
     }
 }
