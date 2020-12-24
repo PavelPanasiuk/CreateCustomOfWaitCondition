6b1f973540703937c94f0b89567b49d82bb8db37
Pavel Panasiuk <pavel.panasiuk.at@gmail.com>
2020-12-24 13:39:53 +0300

    First commit

---
+++ CreateCustomOfWaitCondition/TestCustomDisplayAndEnableClass.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace CreateCustomOfWaitCondition
+{
+    [TestFixture]
+    public class Tests
+    {
+        //Русский текст
+        //English text
+       
+        [SetUp]
+        public void Setup()
+        {
+           
+        }
+
+        [Test]
+        public void Test1()
+        {      
+        }
+
+       
+    }
+}
