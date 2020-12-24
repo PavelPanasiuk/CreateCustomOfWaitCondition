4c8d2f319e832b0c995b5473cad4e8bcbf83c96e
Pavel Panasiuk <pavel.panasiuk.at@gmail.com>
2020-12-24 13:55:49 +0300

    Stop encoding fix

--- CreateCustomOfWaitCondition/TestCustomDisplayAndEnableClass.cs
+++ CreateCustomOfWaitCondition/TestCustomDisplayAndEnableClass.cs
@@ -9,24 +9,18 @@ namespace CreateCustomOfWaitCondition
     [TestFixture]
     public class Tests
     {
-        //Русский текст
-        //English text
-        //Снова русский текст
 
-       
         [SetUp]
         public void Setup()
         {
-            //Русский текст
-            Console.WriteLine("fff");
+            
         }
 
         [Test]
         public void Test1()
-        {    
-            //English text
+        {
         }
 
-       
+
     }
 }
